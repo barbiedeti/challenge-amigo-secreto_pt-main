8593acee1833e0cb2814eae9d15209aa816507d7
Kim Gomes <142019936+cupofkimg@users.noreply.github.com>
2025-01-15 21:14:25 -0300

    v1.0

--- app.js
+++ app.js
@@ -1,1 +1,53 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let nome = "";
+
+//criando função de salvar nome
+function salvarNome() {
+    nome = document.getElementById("nome").value;
+    if (nome == "") {
+        alert("Por favor, insira seu nome");
+    } else {
+        document.getElementById("nome").value = "";
+    }
+}
+
+// criando lista de amigos vazia
+let listaDeAmigos = [];
+
+//criando função de adicionar amigo na lista
+function adicionarAmigo() {
+    let nomeAmigo = document.getElementById("amigo").value;
+    if (nomeAmigo == "") {
+        alert("Por favor, insira um nome");
+    } else {
+        listaDeAmigos.push(nomeAmigo);
+        document.getElementById("amigo").value ="";
+        atualizarLista();
+    }
+}
+
+function atualizarLista() {
+    let listaAtualizada = document.getElementById("listaAmigos");
+    listaAtualizada.innerHTML = "";
+    for (let i = 0; i < listaDeAmigos.length; i++) {
+        let itemLista = document.createElement("li");
+        itemLista.innerHTML = listaDeAmigos[i];
+        listaAtualizada.appendChild(itemLista);
+    }
+}
+
+function sortearAmigo() {
+    if (listaDeAmigos.length == 0) {
+        alert("Por favor, insira um nome.");
+        return;
+
+    }
+    let indiceAleatorio = Math.floor(Math.random() * listaDeAmigos.length);
+    let nomeSorteado = listaDeAmigos[indiceAleatorio];
+    if (nomeSorteado == nome) {
+        alert("Você não pode ser seu próprio amigo secreto! Por favor, tente novamente.");
+        return;
+    }
+
+    document.getElementById("resultado").innerHTML = `Seu amigo secreto é: ${nomeSorteado}`;
+
+}  
